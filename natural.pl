1ecb6e339682352f0021991ebc6a3b8b0ec594e6
CristianColavito <cristian.colavito@gmail.com>
2020-03-04 14:20:11 +0100

    Add files via upload

---
+++ natural.pl
@@ -0,0 +1,23 @@
+/*
+
+Mathematical induction
+
+1) P(n0)
+2) n > n0, P(n-1) -> P(n)
+
+- Program to check if any number is natural applying mathematical induction
+
+  Natural(Number)
+  is true if Number is a natural number (1,2,3, ...)
+
+  natural(1).
+  
+  for n>1, natural(n-1) -> natural(n)
+  a->b
+
+  b:-a.
+	
+*/
+
+natural(1).
+natural(N):- N > 1, N2 is N-1, natural(N2).
